f3cd23b6cdc7a43f794fab221f86cea59c7b3398
Anifu-ngugi <anifngugi1@gmail.com>
2025-02-16 13:20:21 +0300

    Added import pysqlite3-library

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 import chromadb
 import ollama
-import pysqlite3 as sqlite3
+import pysqlite3 as sqlite3 #maked changes
 import streamlit as st
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from langchain_community.document_loaders import PyMuPDFLoader
